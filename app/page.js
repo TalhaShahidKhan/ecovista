27eb47e4286661ee8aec09083afb83fc0f72f3af
Talha Shahid Khan <talha@HackingLab>
2025-09-19 17:34:48 +0600

    Next Weather Dashboard

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
 import LocationDetector from "@/components/LocationDetector";
+import { Suspense } from "react";
 
 export default function page() {
-  return <LocationDetector />;
+  return (
+    <Suspense
+      fallback={
+        <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-white my-4"></div>
+      }
+    >
+      <LocationDetector />
+    </Suspense>
+  );
 }
